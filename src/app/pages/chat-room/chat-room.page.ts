b8436652e757079b6c3e42b17e91fa8e3d7457e0
JoseCarden <josejesuscardenasjimenez@gmail.com>
2024-06-04 21:11:16 -0500

    Inicio de chat
    Chat prototipo, aun falta afinar

--- src/app/pages/chat-room/chat-room.page.ts
+++ src/app/pages/chat-room/chat-room.page.ts
@@ -2,6 +2,8 @@ import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Socket } from 'ngx-socket-io';
 import { ToastController } from '@ionic/angular';
+import { UserData } from '../../interfaces/intChat/UserData';
+import { Message } from 'src/app/interfaces/intChat/Message';
 
 @Component({
   selector: 'app-chat-room',
@@ -10,9 +12,9 @@ import { ToastController } from '@ionic/angular';
 })
 export class ChatRoomPage implements OnInit, OnDestroy{
 
-  messages = [];
-  nickname = '';
-  message = '';
+  messages: Message[] = [];
+  nickname: string | null = '';
+  message: string = '';
 
   constructor(
     private route: ActivatedRoute,
@@ -21,11 +23,33 @@ export class ChatRoomPage implements OnInit, OnDestroy{
   ) { }
 
   ngOnInit() {
-    
+    this.nickname = localStorage.getItem('estu');
+
+    this.socket.on('message', (message: Message) => this.messages.push(message));
+
+    this.socket.on('users-changed', (data: UserData) => {
+      const user = data['user'];
+      if (data['event'] === 'left') {
+        this.showToast('User left: ' + user);
+      } else {
+        this.showToast('User joined: ' + user);
+      }
+    });
+  }
+  sendMessage() {
+    this.socket.emit('add-message', { text: this.message });
+    this.message = '';
   }
 
   ngOnDestroy() {
-    
+    this.socket.disconnect();
   }
 
+  async showToast(msg: string) {
+    const toast = await this.toastCtrl.create({
+      message: msg,
+      duration: 2000
+    });
+    toast.present();
+  }
 }
